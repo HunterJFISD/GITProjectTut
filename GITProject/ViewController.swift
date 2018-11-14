bd9d48b3de1a33bd93cf07b561bad7957ec7e349
Hunter Johnston <130113@s020rmc148-11.local>
2018-11-14 09:50:01 -0600

    Initial Commit

---
+++ GITProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GITProject
+//
+//  Created by Johnston, Hunter P on 11/14/18.
+//  Copyright © 2018 Johnston, Hunter P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
